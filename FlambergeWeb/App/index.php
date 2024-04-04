a8ccad24d895a79baecacdffc1dd6c5804cb2afa
LaMeteoDuBitcoin <quere.quilele.974@gmail.com>
2024-04-04 09:47:16 +0200

    Police carroussel

--- FlambergeWeb/App/index.php
+++ FlambergeWeb/App/index.php
@@ -10,6 +10,7 @@
   <link rel="stylesheet" href="carousel.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <link href='https://fonts.googleapis.com/css?family=Biryani' rel='stylesheet'>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21"></script>
   <script src="./sidescroll.js"></script>
